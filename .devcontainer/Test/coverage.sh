6cc5c212ef7b53cf2a16b5e67b0d0a3301c29a92
slyaac <grandmasterbpl@gmail.com>
2025-04-03 10:55:37 +0000

    cov script update

--- .devcontainer/Test/coverage.sh
+++ .devcontainer/Test/coverage.sh
@@ -4,12 +4,12 @@
 # Run build commands
 echo "#### Starting coverage process..."
 
-rm -rf out/
-rm -rf *.gcov
-rm -rf *.info
-gcov test.cpp -o ./build/CMakeFiles/myCode.dir/test.cpp.gcno
-lcov --capture --directory . --output-file coverage.info
+rm -rf cov/
+mkdir cov
+cd cov
+gcov ../test.cpp -o ../build/CMakeFiles/myCode.dir/test.cpp.gcno
+lcov --capture --directory ../ --output-file coverage.info
 genhtml coverage.info --output-directory out
-
+cd ..
 # Notify completion
 echo "#### Coverage process completed!"
